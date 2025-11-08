c0d919c6591fa985d545d14bd131634c5aa3855e
guilherme1r2g-cmd <guilherme1r2g@gmail.com>
2025-11-08 18:54:28 -0300

    feat: add text readme

---
+++ exercicioidade.py
@@ -0,0 +1,8 @@
+nome = input("Qual o seu nome? ")
+idade = int(input("Qual a sua idade? "))
+
+if idade >= 18:
+    print(f"Olá {nome}, você é de maior")
+
+else:
+    print(f"Olá {nome}, você é de menor")
