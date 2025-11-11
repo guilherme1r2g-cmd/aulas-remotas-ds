9143b8a55807c41d6f373f4223c2ebf2395ab6ad
guilherme1r2g-cmd <guilherme1r2g@gmail.com>
2025-11-11 20:12:33 -0300

    feat: add text

--- funcao_calculadora.py
+++ funcao_calculadora.py
@@ -1,1 +1,14 @@
-
+def calculadora(numero1, numero2, operacao):
+    if operacao == "+":
+        return numero1 + numero2
+    elif operacao == "-":
+        return numero1 - numero2
+    elif operacao == "*":
+        return numero1 * numero2
+    elif operacao == "/":
+        if numero2 != 0:
+            return numero1 / numero2
+        else:
+            return "Erro: divisão por zero"
+    else:
+        return "Operação inválida"
